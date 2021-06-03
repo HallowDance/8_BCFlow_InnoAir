8002b4d22831eba67ea83518938af7fc7c9e00d2
HallowDance <ivo.n.iliev@abv.bg>
2021-06-03 15:23:46 +0300

    added draft of interpolation function

--- evaluateLines.py
+++ evaluateLines.py
@@ -1,10 +1,9 @@
 # In this file we take the mock data structures and give a score to each bus
 # line. The score depends on the sum of pollution scores of all the squares the
 # line traverses.
-
 import numpy as np
 from squares import mapPlot
-
+from interpolate import interpolate
 gridSize=10
 pollutionValues=np.zeros(gridSize**2)
 linesDictSquares={} #dictionary containing line names as keys, and routes as values
@@ -15,16 +14,30 @@ with open("buslines.dat", "r") as f:
         li=line.strip()
         if not li.startswith('#'):
             linesDictSquares[li.split(' ')[0]]=li.split(' ')[1:]
-print(linesDictSquares)
-# get pollution info on each 
+
+
+# get pollution info on each
+noDataSquares=[] # to contain squares with no data (for interpolation)
 with open("squares.dat", "r") as f:
     i=0
     for line in f:
         li=line.strip()
         if not li.startswith('#'):
-            #print(li)
-            pollutionValues[i]=li
+            try:
+                #if there exeists a data point for that square
+                pollutionValues[i]=li
+            except:
+                #if it doesn't exist insert NaN and save the index of the
+                #square. We will assume an intepolated value later
+                if(li==''):
+                    noDataSquares.append(i)
+                    pollutionValues[i]=np.nan
             i+=1
+#call interpolation function for missing data
+for square in noDataSquares:
+    pollutionValues[square]=interpolate(gridSize,square,pollutionValues)
+    print(pollutionValues[square])
+
 
 # evaluate pollution levels on each line (this has to be reimplemented better)
 for key in linesDictSquares:
@@ -34,6 +47,6 @@ for key in linesDictSquares:
     for element in squaresList:
         linePollutionLevel+=pollutionValues[int(element)]
     linesDictPolution[key]=linePollutionLevel
-#print(linesDictPolution)
+print(linesDictPolution)
 
-mapPlot(pollutionValues, linesDictSquares, gridSize)
+mapPlot(pollutionValues, gridSize)
